4b8f6b81f31753fc85ce3265c0d1ea3bd785e540
Oliver Dahl <oliverpdahl@gmail.com>
2019-12-31 15:44:25 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -12,11 +12,10 @@
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
 def speak_to_grandma(string)
-
-end
-  
   if string.upcase
     nil 
   else
     return "HUH?! SPEAK UP, SONNY!"
   end
+end
+  
