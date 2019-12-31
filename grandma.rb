acd29c094908586c97f58cd8c9d60d31fd99aef1
Oliver Dahl <oliverpdahl@gmail.com>
2019-12-31 15:46:09 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -16,7 +16,7 @@ def speak_to_grandma(string)
     if string == "I LOVE YOU GRANDMA!"
       "I LOVE YOU TOO PUMPKIN!"
     else
-      
+      "NO, NOT SINCE 1938!"
   else
     "HUH?! SPEAK UP, SONNY!"
   end
